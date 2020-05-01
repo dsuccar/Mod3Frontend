6f55f139af70914c15cbc7619c00d72c2c28c782
kaidornel <kai_dornel@hotmail.com>
2020-05-01 17:53:05 -0400

    finished basic functionality

--- src/index.js
+++ src/index.js
@@ -4,28 +4,26 @@ const USERQUESTIONS_URL = `${BASE_URL}/user_questions`
 const USERS_URL = `${BASE_URL}/users`
 let page = 1;
 let userId = 1
-// const flatten = require('lodash.flatten')
-function addListeners(){ //didn't work to fix multiple firing bug
-  const correct = document.querySelector(".correct-answer")
-  correct.addEventListener("click", (event) => submitAnswer(event))
+let globalPoints = 0
 
-  const incorrect = document.querySelectorAll(".incorrect-answer")
-  incorrect.forEach((el) => {el.addEventListener("click", (event) => incorrectAns(event))})
-}
+// function stringFixer1(string){
+//   return string.replace(/&quot;/g, "'")
+// }
 
+// function stringFixer2(string){
+//   return string.replace(/&#039;/g, "'")
+// }
+
+// function stringFixer3(string){
+//   return string.replace(/&amp/g, "&")
+// }
 
 document.addEventListener("DOMContentLoaded", () => {
   // fetchQuestions()
-  renderMain()
+  fetchUsers()
+  // renderMain()
   // addListeners()
 })
-
-// function fetchQuestions() {
-//   console.log("Loading questions")
-//   fetch(QUESTIONS_URL + `?_limit=1&_page=${page}`)
-//     .then(resp => resp.json())
-//     .then(questionsArr => renderMain(questionsArr))
-// }
 
 function fetchQuestions() {
   console.log("Loading questions")
@@ -35,32 +33,54 @@ function fetchQuestions() {
     // .then(json => json.forEach(question => renderQuestions(question)))
 }
 
-function renderMain(){
+function fetchUsers(){
+  console.log("Retrieving Danny's total score")
+  fetch(USERS_URL)
+  .then(resp => resp.json())
+  .then(user => renderMain(user))
+}
+
+function renderMain(user){
   const h1 = document.createElement("h1")
   h1.innerText = "Welcome! Click here to begin."
   const main = document.querySelector(".main")
-  main.append(h1)
+
+  const p = document.createElement("p")
+  p.classList.add("leaderboard")
+  p.innerText = `${user[0].name}: ${user[0].user_questions.length} points`
+  // debugger
+  const input = document.createElement("input")
+  input.placeholder = "Log in here..."
+  main.append(h1, p)
 
   h1.addEventListener("click", () => fetchQuestions())
 }
 
 function renderQuestions(question) {
-  globalQuestion = question
-  // function getRandomInt(max) {
-  //   return Math.floor(Math.random() * Math.floor(max));
-  // }
+  globalQuestion = question //intentional global variable
+  const points = document.querySelector(".round-points")
+  points.innerText = `Points: ${globalPoints}`
+
   let randomQuestion = question[Math.floor(Math.random() * question.length)]//Math.floor(Math.random() * Math.floor(max));
  
   console.log("Rendering question for user")
 
   const mainTitle = document.querySelector("h1")
+  leaderboard = document.querySelector(".leaderboard")
 
   mainTitle.style.display = "none"
+  leaderboard.style.display = "none"
 
   const questionContainer = document.getElementById("question-container")
   questionContainer.innerHTML = ""
   const questionText = document.createElement("h3")
   questionText.classList.add("question-text")
+  let dataText = randomQuestion.question_text
+  console.log(dataText) //string
+  
+  // let dataText1 = stringFixer1(dataText)
+  // let dataText2 = stringFixer2(dataText1)
+  // questionText.innerText = dataText2
   questionText.innerText = randomQuestion.question_text
 
   questionContainer.append(questionText)
@@ -83,8 +103,12 @@ function renderQuestions(question) {
     answer.classList.add("answer")
     answer.innerText = answersArr[i]
     answerContainer.append(answer)
+    if(answersArr[i] === randomQuestion.correct_answer) {
+      answer.setAttribute("id", "correct-answer")
+    }else{
+      answer.setAttribute("id", "incorrect-answer")
+    }
   }
-  // debugger
 
   function getRandomInt(max) {
     return Math.floor(Math.random() * Math.floor(max));
@@ -92,10 +116,23 @@ function renderQuestions(question) {
   
   
   for (let i = answerContainer.children.length; i >= 0; i--) {
-        answerContainer.appendChild(answerContainer.children[getRandomInt(4)]);
+    // debugger
+        answerContainer.appendChild(answerContainer.children[getRandomInt(answersArr.length)]); //4
   }
-    
-  
+
+  const correct = document.getElementById("correct-answer")
+  correct.addEventListener("click", (event) => submitAnswer(event, randomQuestion))
+
+  const incorrect = document.querySelectorAll("#incorrect-answer")
+  incorrect.forEach((el) => {el.addEventListener("click", (event) => incorrectAns(event))})
+
+  // debugger
+  // const correct = document.querySelector(".correct-answer")
+  // 
+
+  // const incorrect = document.querySelectorAll(".incorrect-answer")
+  // incorrect.forEach((el) => {el.addEventListener("click", (event) => incorrectAns(event))})
+
 
   // so answers are on bottom
 
@@ -128,6 +165,15 @@ function renderQuestions(question) {
 function incorrectAns(event){
   event.preventDefault()
 
+  const bigGreenCheckmark = document.querySelector(".big-green-checkmark")
+  bigGreenCheckmark.innerText = ""
+
+  // const previousQuestion = event.target.parentNode.parentNode
+  const h2 = document.querySelector("h2")
+  const redX = document.querySelector(".big-red-x")
+  redX.innerText = "❌"
+  h2.append(redX)
+
   console.log("Success!")
   //next question
   renderQuestions(globalQuestion)
@@ -135,9 +181,21 @@ function incorrectAns(event){
 
 function submitAnswer(event, randomQuestion) {
   event.preventDefault()
-  console.log("submitting")
-  // if (event.target.innerText === question.correct_answer) {
+  
+  const points = document.querySelector(".round-points")
+  points.innerText = `Points: ${++globalPoints}`
 
+  const bigRedX = document.querySelector(".big-red-x")
+  bigRedX.innerText = ""
+
+  const h2 = document.querySelector("h2")
+  const greenCheckmark = document.querySelector(".big-green-checkmark")
+  h2.append(greenCheckmark)
+  greenCheckmark.innerText = "✅"
+
+  console.log("submitting")
+
+    // alert("Correct Answer!")
     obj = {
       question_id: randomQuestion.id,
       user_id: userId
@@ -152,14 +210,4 @@ function submitAnswer(event, randomQuestion) {
       body: JSON.stringify(obj)
     })
     renderQuestions(globalQuestion)
-    // renderQuestions(question)
-    // .then(resp => resp.json())
-    // .then(submit => renderQuestions(submit))
-    // div.append(ul)
-    // container.append(div)
-  // }
-  // else {
-  //   console.log("nope")
-  // }
-  //next question
 }
